fbf1ebb357337e635681569561076e0961c93698
mrknrb <mrknrb2@gmail.com>
2022-07-05 12:06:21 +0200

    3

--- src/Components/TopBar.svelte
+++ src/Components/TopBar.svelte
@@ -2,7 +2,7 @@
     import {saveDataMainStoreReducers} from "../Store/StoreSaveData"
 </script>
 
-<div style="background-color: #4f4f4f;height: 50px;width: 100%">
+<div style="background-color: #3c70b4;height: 50px;width: 100%">
 
 <input type="button" value="Save" class="bg-amber-400 h-10 w-10 hover:bg-amber-500 font-bold float-right"
 on:click={()=>{saveDataMainStoreReducers.saveData()}}
