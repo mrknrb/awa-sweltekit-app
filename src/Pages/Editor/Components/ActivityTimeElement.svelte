a7787736568df66fb1b89c5efc8cfd23547b0f91
mrknrb <mrknrb2@gmail.com>
2022-10-26 15:10:13 +0200

    13

--- src/Pages/Editor/Components/ActivityTimeElement.svelte
+++ src/Pages/Editor/Components/ActivityTimeElement.svelte
@@ -12,13 +12,18 @@
 		if (hovered) return 'brightness-110';
 		return '';
 	};
+	$: imageurl = 'images/' + activitySaveData.activityType + '.jpg';
 </script>
 
-<div
-	on:mouseenter
-	on:mouseleave
-	style="min-height:{nagyitasKalkulaloEredmeny}rem;height: {nagyitasKalkulaloEredmeny}rem"
-	class="w-16 bg-green-600 flex justify-center cent overflow-hidden border-b-4 {brighterOnHover()} "
->
-	<a class="text-center">{activitySaveData.activityType}</a>
+<div style="  background-image: linear-gradient(#bbbbbb, #858585)">
+	<div
+		on:mouseenter
+		on:mouseleave
+		style="min-height:{nagyitasKalkulaloEredmeny}rem;height: {nagyitasKalkulaloEredmeny}rem ;background-image: url({imageurl});background-repeat: no-repeat ;background-size:180%;background-position: -50px"
+		class="w-16  flex justify-center cent overflow-hidden {brighterOnHover()} "
+	>
+		<div style=" backdrop-filter:blur(2px) ;width: 100%;height: 100%">
+			<a class="text-center">{activitySaveData.activityType}</a>
+		</div>
+	</div>
 </div>
